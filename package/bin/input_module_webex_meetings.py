596e6f53a72f0ffd8f66cf9f9d338e3e343992da
Yuan Ling <yling@splunk.com>
2025-06-18 17:40:07 -0400

    fixed the timeout issue for the list people endpoint by lowing the max to _MAX_PAGE_SIZE(100)

--- package/bin/input_module_webex_meetings.py
+++ package/bin/input_module_webex_meetings.py
@@ -118,7 +118,7 @@ def collect_events(helper, ew):
         )
 
     # get user list
-    people_params = {"max": 1000}
+    people_params = {}
     users = paging_get_request_to_webex(
         helper,
         base_endpoint,
